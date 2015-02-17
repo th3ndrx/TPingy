179018ea4dcd36371f5f4d1ebe1bed8dc01d0159
Tomislav <tomislav.sablic@outlook.com>
2015-02-17 16:43:36 +0100

    Commit

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+__author__ =    "Tomislav Sablic"
+__copyright__ = "Copyright 2015, Tomislav Sablic"
+__license__ =   "MIT"
+__version__ =   "1.0"
+__email__ =     "tomislav.sablic@outlook.com"
+__status__ =    "Development" 
+
+######################## Twitter API auth ##############################
+consumer_key    =     '5NPIYybdVPn8gVDwjAiXDZgFs'
+consumer_secret =     'wSx5JxKIcwHAwSDNpblTF2dzPnioRBnGJsxsnnExCJxMyLTcyK'
+access_token    =     '1112800934-aKpdGP3bKD99l9Iu4NLKGgZznufcOIjo350nRS0'
+access_token_secret = 'ITElwfSAOSsufFv9irJQj6BY2KUN5zjPqDF1NJXT9jc1G'
+appuser =             '1112800934'
+########################################################################
+
+hostname = "nope.gif"
+alert    = "There is something going on with %s , you should check it." % hostname
+  
+    
